2431c29b91bb56ba2c9e3844e9e26620d1f55891
Fahim-rafi24 <fahimrafibd2003@gmail.com>
2026-08-01 21:02:02 +0600

    update next server setup for my version next-14v

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'localhost',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
